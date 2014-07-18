cf0733881650a075a40b47b1761028d70d0565c5
Martin Simka <msimka@redhat.com>
2014-07-18 16:01:09 +0200

    add custom krb5.conf, not use system one

--- src/main/java/krb/test/Main.java
+++ src/main/java/krb/test/Main.java
@@ -13,21 +13,22 @@ import java.util.Properties;
  */
 public class Main {
     // postgresql 9.2
-//    private static final String URL = "jdbc:postgresql://db14.mw.lab.eng.bos.redhat.com:5432/dballo01";
+    private static final String URL = "jdbc:postgresql://db14.mw.lab.eng.bos.redhat.com:5432/krbusr01";
     // postgresql plus
     //private static final String URL = "jdbc:edb://db15.mw.lab.eng.bos.redhat.com:5432/dballo00";
     //local
-    private static final String URL = "jdbc:postgresql://krbtest:5432/template1";
+//    private static final String URL = "jdbc:postgresql://krbtest:5432/template1";
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("java.security.krb5.realm", "JBOSS.ORG");
-        System.setProperty("java.security.krb5.kdc", "krbtest");
-//        System.setProperty("java.security.krb5.realm", "MW.LAB.ENG.BOS.REDHAT.COM");
-//        System.setProperty("java.security.krb5.kdc", "kerberos-test.mw.lab.eng.bos.redhat.com");
+//        System.setProperty("java.security.krb5.realm", "JBOSS.ORG");
+//        System.setProperty("java.security.krb5.kdc", "krbtest");
+        System.setProperty("java.security.krb5.realm", "MW.LAB.ENG.BOS.REDHAT.COM");
+        System.setProperty("java.security.krb5.kdc", "kerberos-test.mw.lab.eng.bos.redhat.com");
         System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("java.security.krb5.conf", "/home/msimka/Projekty/redhat/tmp/jdbc-client/krb5.conf");
 
-//        System.setProperty("java.security.auth.login.config", "/home/msimka/Projekty/redhat/tmp/jdbc-client/login.conf"); // EDIT PATH!!
-        System.setProperty("java.security.auth.login.config", "/home/msimka/Projekty/redhat/tmp/jdbc-client/login_local.conf"); // EDIT PATH!!
+        System.setProperty("java.security.auth.login.config", "/home/msimka/Projekty/redhat/tmp/jdbc-client/login.conf"); // EDIT PATH!!
+//        System.setProperty("java.security.auth.login.config", "/home/msimka/Projekty/redhat/tmp/jdbc-client/login_local.conf"); // EDIT PATH!!
 
         Subject specificSubject = new Subject();
         LoginContext lc = new LoginContext("pgjdbc", specificSubject);
@@ -58,9 +59,10 @@ public class Main {
                     {
                         Connection con = null;
                         Properties prop = new Properties();
-//                        prop.setProperty("user", "KRBUSR01");
-                        prop.setProperty("user", "host/eap@JBOSS.ORG");
+                        prop.setProperty("user", "KRBUSR01");
+//                        prop.setProperty("user", "host/eap@JBOSS.ORG");
                         prop.setProperty("jaasApplicationName", "pgjdbc");
+                        //prop.setProperty("kerberosServerName", "aaa");
                         String url = URL;
                         try
                         {
